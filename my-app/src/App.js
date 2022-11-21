3d7f94c3586faec9af8d870a612869d04f124d59
Roman <roman@Romans-MacBook-Air.local>
2022-11-21 15:58:24 +0200

    test1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Version 00.
+          Edit <code>src/App.js</code> and save to reload Version 100.
         </p>
         <a
           className="App-link"
